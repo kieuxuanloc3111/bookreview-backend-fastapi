c33d70f713c08d5fe139333aff30b889f761c4fe
kieuxuanloc3111 <chokeslam311@gmail.com>
2026-04-21 16:57:15 +0700

    database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/book_reviews"
+DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/book_reviews"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
