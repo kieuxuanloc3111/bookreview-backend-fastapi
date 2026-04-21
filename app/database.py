ce943b8391882161c9e1cf31596ffe72e0269ee7
kieuxuanloc3111 <chokeslam311@gmail.com>
2026-04-21 17:06:12 +0700

    database

--- app/database.py
+++ app/database.py
@@ -3,6 +3,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://", "postgresql+psycopg2://", 1
+    )
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 # DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/book_reviews"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
